96f90ad9fc2360607f4b4f12775bc98cd6107180
liangliang <liangliang@liangliang-Aspire-V5-471G.(none)>
2013-12-29 15:41:32 +0700

    update on 2013/12/29

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,7 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid 
+
 
   # GET /carts
   # GET /carts.json
@@ -54,9 +56,10 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url }
+      format.html { redirect_to store_url, notice: "Your currently cart now is empty." }
       format.json { head :no_content }
     end
   end
@@ -66,7 +69,11 @@ class CartsController < ApplicationController
     def set_cart
       @cart = Cart.find(params[:id])
     end
-
+  def invalid
+      logger.error "try to access #{params[:id]}"
+      redirect_to store_url, notice: "Invalid cart"
+ 
+  end
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params[:cart]
